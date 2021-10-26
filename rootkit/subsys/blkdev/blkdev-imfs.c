ca7f275f56c91eb523aa00a3244fb03cdd127aa0
wt <wt1454246140@163.com>
2021-10-26 20:05:43 +0800

    Updated

--- rootkit/subsys/blkdev/blkdev-imfs.c
+++ rootkit/subsys/blkdev/blkdev-imfs.c
@@ -11,19 +11,45 @@
  * found in the file LICENSE in this distribution or at
  * http://www.rtems.org/license/LICENSE.
  */
-#include <sys/stat.h>
+
 #include <stdlib.h>
 #include <string.h>
+
+#include <kernel.h>
 #include <device.h>
+
+struct k_blkdev_partition {
+  const char *partition;
+  const char *devname;
+  size_t start;
+  size_t size;
+};
 
 struct k_blkdev_context {
   struct k_disk_device dd;
   const struct device *dev;
+  atomic_t refcnt;
+  struct k_blkdev_context *next;
+  const struct k_blkdev_partition *p;
 };
 
-static ssize_t k_blkdev_read(rtems_libio_t *iop, void *buffer,
+extern char __blkdev_partition_start[];
+extern char __blkdev_partition_end[];
+
+static K_MUTEX_DEFINE(blkdev_lock);
+static struct k_blkdev_context *blkdev_list;
+
+
+int k_blkdev_open(const char *partition, struct k_blkdev_context **ctx) {
+
+}
+
+int k_blkdev_close(struct k_blkdev_context *ctx) {
+
+}
+
+ssize_t k_blkdev_read(struct k_blkdev_context *ctx, void *buffer,
   size_t count) {
-  struct k_blkdev_context *ctx = IMFS_generic_get_context_by_iop(iop);
   struct k_disk_device *dd = &ctx->dd;
   ssize_t remaining = (ssize_t)count;
   off_t offset = iop->offset;
@@ -64,9 +90,8 @@ static ssize_t k_blkdev_read(rtems_libio_t *iop, void *buffer,
   return rv;
 }
 
-static ssize_t k_blkdev_write(rtems_libio_t *iop, const void *buffer,
+ssize_t k_blkdev_write(struct k_blkdev_context *ctx, const void *buffer,
   size_t count) {
-  struct k_blkdev_context *ctx = IMFS_generic_get_context_by_iop(iop);
   struct k_disk_device *dd = &ctx->dd;
   ssize_t remaining = (ssize_t)count;
   off_t offset = iop->offset;
@@ -111,64 +136,21 @@ static ssize_t k_blkdev_write(rtems_libio_t *iop, const void *buffer,
   return rv;
 }
 
-static int k_blkdev_ioctl(rtems_libio_t *iop, ioctl_command_t request,
-  void *buffer) {
+int k_blkdev_ioctl(struct k_blkdev_context *ctx, 
+  ioctl_command_t request, void *buffer) {
   int ret = 0;
   if (request != RTEMS_BLKIO_REQUEST) {
-    struct k_blkdev_context *ctx = IMFS_generic_get_context_by_iop(iop);
     struct k_disk_device *dd = &ctx->dd;
     ret = (*dd->ioctl)(dd, request, buffer);
   } else {
-    /*
-     * It is not allowed to directly access the driver circumventing the cache.
-     */
     errno = EINVAL;
     ret = -1;
   }
-
   return ret;
 }
 
-static int rtems_blkdev_imfs_fstat(
-  const rtems_filesystem_location_info_t *loc,
-  struct stat *buf
-)
-{
-  struct k_blkdev_context *ctx;
-  struct k_disk_device *dd;
-  IMFS_jnode_t *node;
 
-  ctx = IMFS_generic_get_context_by_location(loc);
-  dd = &ctx->dd;
-
-  buf->st_blksize = rtems_disk_get_block_size(dd);
-  buf->st_blocks = rtems_disk_get_block_count(dd);
-
-  node = loc->node_access;
-  buf->st_rdev = IMFS_generic_get_device_identifier_by_node(node);
-
-  return IMFS_stat(loc, buf);
-}
-
-static int rtems_blkdev_imfs_fsync_or_fdatasync(
-  rtems_libio_t *iop
-)
-{
-  int rv = 0;
-  struct k_blkdev_context *ctx = IMFS_generic_get_context_by_iop(iop);
-  struct k_disk_device *dd = &ctx->dd;
-  rtems_status_code sc = rtems_bdbuf_syncdev(dd);
-
-  if (sc != RTEMS_SUCCESSFUL) {
-    errno = EIO;
-    rv = -1;
-  }
-
-  return rv;
-}
-
-
-int k_blkdev_create(const char *device, uint32_t media_block_size,
+static int k_blkdev_create(const char *device, uint32_t media_block_size,
   blkdev_bnum_t media_block_count, blkdev_ioctrl_fn handler,
   void *driver_data) {
   struct k_blkdev_context *ctx;
@@ -196,30 +178,4 @@ _freem:
   return ret;
 }
 
-int k_blkdev_create_partition(const char *partition,
-  const char *parent_block_device, blkdev_bnum_t media_block_begin,
-  blkdev_bnum_t media_block_count) {
-  struct k_disk_device *phys_dd;
-  struct k_blkdev_context *ctx;
-  struct device *dev;
-  int ret;
 
-  dev = device_get_binding(parent_block_device);
-  if (dev == NULL)
-    return -ENODEV;
-  ret = k_disk_fd_get_disk_device(fd, &phys_dd);
-  if (ret)
-    return -ESRCH;
-  ctx = k_malloc(sizeof(*ctx));
-  if (ctx == NULL）
-    return -ENOMEM;
-  ret = k_disk_init_log(&ctx->dd, phys_dd, media_block_begin, 
-    media_block_count);
-  if (ret) {
-    k_free(ctx);
-    goto _exit;
-  }
-  ctx->dev = dev;
-_exit:
-  return ret; 
-}
