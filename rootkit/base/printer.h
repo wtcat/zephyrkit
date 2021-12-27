567aaa929513c61a2b0c2a55395aab04af2d7ef1
wt <wt1454246140@163.com>
2021-12-27 22:31:22 +0800

    Updated

---
+++ rootkit/base/printer.h
@@ -0,0 +1,26 @@
+#ifndef BASE_PRINTER_H_
+#define BASE_PRINTER_H_
+
+#include <stdarg.h>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+
+struct printer {
+    void (*print)(void *context, const char *fmt, va_list ap_list);
+    void *context;
+};
+
+static inline int virt_print(struct printer *printer, const char *fmt, ...) {
+    va_list ap;
+    va_start(ap, fmt);
+    int len = printer->print(printer->context, fmt, ap);
+    va_end(ap);
+    return len;
+}
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* BASE_PRINTER_H_ */
